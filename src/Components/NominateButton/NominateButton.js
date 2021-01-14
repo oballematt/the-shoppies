c084a091a0fb7966013a1e8107a6471eac2824d6
Matthew Oballe <oballematt@gmail.com>
2021-01-14 12:23:55 -0600

    added disbaled btton to all movies once nominations length is equal to 5.

--- src/Components/NominateButton/NominateButton.js
+++ src/Components/NominateButton/NominateButton.js
@@ -3,6 +3,18 @@ import React from 'react'
 const NominateButton = (props) => {
     let isNominated = props.nominations.filter(nomination => nomination.imdbID === props.movie.imdbID).length !== 0
 
+    if (props.nominations.length === 5) {
+        return (
+            <button style={{ backgroundColor: "green", color: "white" }} disabled className="overlay d-flex align-items-center justify-content-center btn btn-outline-secondary">
+                <div>
+                    <span className="mr-2 font">Nominations Full</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                    </svg>
+                </div>
+            </button>
+        )
+    }
     if (!isNominated) {
         return (
             <button onClick={() => props.handleNominationsClick(props.movie)} className="overlay d-flex align-items-center justify-content-center btn btn-outline-secondary">
@@ -25,8 +37,8 @@ const NominateButton = (props) => {
                 </div>
             </button>
         )
-   }
-
+   } 
+   
 }
 
 export default NominateButton
