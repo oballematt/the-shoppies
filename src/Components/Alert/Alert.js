78d33c6f49dfa81d3ac3b4969df3d2121b3e5cf6
Matthew Oballe <oballematt@gmail.com>
2021-01-14 01:42:40 -0600

    making sure all font styling is uniform.

--- src/Components/Alert/Alert.js
+++ src/Components/Alert/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Alert = () => {
     return (
-        <div className="alert alert-success" id="alert" role="alert">
+        <div className="alert alert-success font " style={{color: "black"}} id="alert" role="alert">
             <strong>You have nominated 5 movies!</strong>
         </div>
     )
