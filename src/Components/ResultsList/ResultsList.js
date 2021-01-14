392986a200a0a5595f2432b8e087f6da2acc2d8a
Matthew Oballe <oballematt@gmail.com>
2021-01-14 00:17:24 -0600

    disbaled nomination button once a movie is clicked and added to nomations list. Styled the disabled button.

--- src/Components/ResultsList/ResultsList.js
+++ src/Components/ResultsList/ResultsList.js
@@ -2,14 +2,14 @@ import React from "react"
 
 const ResultsList = (props) => {
     const NominateButton = props.nominateButton
+    const nominations = props.nominations
+  
     return (
        <>
        {props.movies.map((movie, index) =>
         <div className=" image-container d-flex justify-content-center align-items-center m-3">
            <img src={movie.Poster} alt="movie-poster"></img>
-           <button  onClick={() => props.handleNominationsClick(movie)} className="overlay d-flex align-items-center justify-content-center btn btn-outline-secondary">
-               <NominateButton  />
-           </button>
+               <NominateButton nominations={nominations} movie={movie} handleNominationsClick={props.handleNominationsClick} />
        </div>
        )}
        </>
